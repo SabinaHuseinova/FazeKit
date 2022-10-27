f4545c55b71840e8ad5ad6ba4b01d8bbd2c3e2b8
SabinaHuseinova <116122861+SabinaHuseinova@users.noreply.github.com>
2022-10-27 22:23:27 +0300

    Update UIApplicationAdditions.swift

--- FazeKit/Classes/UIApplicationAdditions.swift
+++ FazeKit/Classes/UIApplicationAdditions.swift
@@ -10,8 +10,8 @@ import Foundation
 private var activityCount: Int = 0
 
 public extension UIApplication {
-	#if os(iOS)
-	static func incrementNetworkActivityIndicator() {
+    #if os(iOS)
+    static func incrementNetworkActivityIndicator() {
         DispatchQueue.main.async {
             activityCount += 1
             self.shared.isNetworkActivityIndicatorVisible = true
@@ -24,7 +24,7 @@ public extension UIApplication {
             self.shared.isNetworkActivityIndicatorVisible = activityCount > 0
         }
     }
-	#endif
+    #endif
     
     static var appVersionString: String {
         return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
